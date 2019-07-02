2f38f8ae746273503631cf95f510036adeae2a05
blackpanda4 <51849284+blackpanda4@users.noreply.github.com>
2019-07-02 06:30:36 +0300

    Add files via upload

---
+++ session2.py
@@ -0,0 +1,9 @@
+name = input("please enter your name: ")
+
+myFavFruits = ["apple","orange","strawberry"]
+myNumbers = [1,2,3,4,5,6,10,55,44]
+print(myNumbers[4])
+
+for nora in myFavFruits:
+    print(adnan)
+    print
