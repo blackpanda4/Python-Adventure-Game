35dd4b896aa31229d4609b04f9a4ac56ab6881bd
blackpanda4 <51849284+blackpanda4@users.noreply.github.com>
2019-07-10 23:49:54 +0300

    Delete session2.py

--- session2.py
+++
@@ -1,9 +0,0 @@
-name = input("please enter your name: ")
-
-myFavFruits = ["apple","orange","strawberry"]
-myNumbers = [1,2,3,4,5,6,10,55,44]
-print(myNumbers[4])
-
-for nora in myFavFruits:
-    print(adnan)
-    print
